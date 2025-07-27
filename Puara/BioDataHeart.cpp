68f066c1b1cc9b8bbd1a6707393580338958891b
pyandcpp-coder <yashtiwari9182@gmail.com>
2025-07-27 19:05:44 +0530

    bio data_compile issues changes

--- Puara/BioDataHeart.cpp
+++ Puara/BioDataHeart.cpp
@@ -4,8 +4,9 @@ namespace puara_gestures::objects
 {
 void BioData_Heart::operator()(halp::tick_musical t)
 {
-  heart.update(
-      inputs.heart_signal, 1e6 * t.position_in_frames / double(this->setup.rate));
+  // heart.update(
+  //     inputs.heart_signal, 1e6 * t.position_in_frames / double(this->setup.rate));
+  heart.update(inputs.heart_signal);
 
   outputs.heart_raw = heart.getRaw();
   outputs.heart_normalized = heart.getNormalized();
--- Puara/BioDataSkinConductance.cpp
+++ Puara/BioDataSkinConductance.cpp
@@ -4,8 +4,7 @@ namespace puara_gestures::objects
 {
 void BioData_Skin_Conductance::operator()(halp::tick_musical t)
 {
-  sc.update(inputs.sc_signal, 1e6 * t.position_in_frames / double(this->setup.rate));
-
+  sc.update(inputs.sc_signal);
   outputs.sc_raw = sc.getRaw();
   outputs.sc_scr = sc.getSCR();
   outputs.sc_scl = sc.getSCL();
